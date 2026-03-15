6351af77bab8dc241f3b8944017fe91faf3638c0
alexa <alexandre.brianto29@gmail.com>
2026-03-15 13:04:54 +0100

    added popular page

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from json import loads
+import requests
 
 # Create your views here.
 
 def index(request):
-    context = {'name' : 'Alexandre'}
+    context = {"movies" : popularMovies()}
     return render(request, 'pages/index.html', context)
 
-def test(request):
-    return render(request, 'pages/test.html')
+# Functions
+
+def popularMovies():
+    key = "9e43f45f94705cc8e1d5a0400d19a7b7"
+    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={key}&language=fr-FR"
+    result = requests.get(url)
+    return loads(result.text)["results"]
