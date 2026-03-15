0acd7e372134ef08f87d1b1b11468a91fa782e5d
alexa <alexandre.brianto29@gmail.com>
2026-03-15 14:35:40 +0100

    movies by genre done

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         path('', views.popular, name='popular'),
         path('popular', views.popular, name='popular'),
         path('topRated', views.topRated, name='topRated'),
+        path('genreMovies', views.byGenre, name='genreMovies'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -14,6 +14,12 @@ def topRated(request):
     context = {"movies": topRatedMovies()}
     return render(request, 'pages/popular.html', context)
 
+def byGenre(request):
+    id = request.GET.get("id")
+
+    context = {"id" : id, "movies" : moviesByGenre(id)}
+    return render(request, 'pages/genreMovies.html', context)
+
 # Functions
 
 def popularMovies():
@@ -25,6 +31,12 @@ def popularMovies():
 def topRatedMovies():
     key = "9e43f45f94705cc8e1d5a0400d19a7b7"
     url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={key}&language=fr-FR"
+    result = requests.get(url)
+    return loads(result.text)["results"]
+
+def moviesByGenre(id):
+    key = "9e43f45f94705cc8e1d5a0400d19a7b7"
+    url = f"https://api.themoviedb.org/3/discover/movie?api_key={key}&language=fr-FR&with_genres={id}"
     result = requests.get(url)
     return loads(result.text)["results"]
 
