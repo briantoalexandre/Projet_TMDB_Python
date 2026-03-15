62ce95849e58a069dd385ae865101e000033deac
alexa <alexandre.brianto29@gmail.com>
2026-03-15 11:53:16 +0100

    added header and footer

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context = {'name' : 'Alexandre'}
+    return render(request, 'pages/index.html', context)
+
+def test(request):
+    return render(request, 'pages/test.html')
